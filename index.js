0bc35985035617356656f3dd4cb97e1a36ab54b9
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2024-12-10 21:22:27 +0530

    Added env and dotenv

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const { resolve } = require('path');
 
+require('dotenv').config();
+
 const app = express();
 const port = 3010;
+const isAdmin = process.env.IS_ADMIN === 'true';
+
+if (isAdmin) {
+  console.log("Admin privileges granted.");
+} else {
+  console.log("Access restricted. Admin only.");
+}
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  if(isAdmin === 'true'){
+    res.send({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] })
+  }else{
+    res.send({ message: "Welcome, Admin!", data: ["User Data 1", "User Data 2"] })
+  }
 });
 
 app.listen(port, () => {
